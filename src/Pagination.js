f07c5e9e6038bf0e6a5533f210a5569e966aba2c
beingsksharma <30936556+beingsksharma@users.noreply.github.com>
2022-09-04 17:27:48 +0530

    Table width responsiveness

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactPaginate from 'react-paginate';
 import { GrPrevious, GrNext } from 'react-icons/gr';
+import './Pagination.css';
 
 const Pagination = ({pageCount, changePage}) => {
     return (
         <>
         <div className='row'>
           <ReactPaginate
-            previousLabel={<GrPrevious />}
-            nextLabel={<GrNext />}
+            previousLabel={<GrPrevious className="Previous"/>}
+            nextLabel={<GrNext className="Next"/>}
             pageCount={ pageCount }
             onPageChange={ changePage }
             containerClassName={"paginationBttns"}
